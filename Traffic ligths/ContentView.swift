b8aac4be0924b7d870afe18fef7dfa2a6f1bd264
Gennady Krasnoshlyk <gkrasnoshlyk@gmail.com>
2020-10-15 10:09:17 +0500

    Initial Commit

---
+++ Traffic ligths/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Traffic ligths
+//
+//  Created by Aquesta 's on 15.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Traffic ligths/Traffic_ligthsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Traffic_ligthsApp.swift
+//  Traffic ligths
+//
+//  Created by Aquesta 's on 15.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct Traffic_ligthsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
